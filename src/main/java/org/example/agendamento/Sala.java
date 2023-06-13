5faafd6da815ed5b72d4b204cfc3be43484b1981
Kamila <kamila.cerdeira.kc@gmail.com>
2023-06-13 14:37:12 -0300

    cria o tipo e interface da Sala

---
+++ src/main/java/org/example/agendamento/Sala.java
@@ -0,0 +1,7 @@
+package org.example.agendamento;
+
+public interface Sala {
+    String getId();
+    String getNome();
+    SalaType getTipo();
+}
---
+++ src/main/java/org/example/agendamento/SalaType.java
@@ -0,0 +1,7 @@
+package org.example.agendamento;
+
+public enum SalaType {
+    SALA_DE_JOGOS,
+    SALA_DE_REUNIAO,
+    AUDITORIO
+}
